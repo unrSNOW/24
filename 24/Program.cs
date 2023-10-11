1eba95fe868bdc98fbc52f070dda8bf12004997b
Asus <Asus@LAPTOP-4QTPOURE>
2023-10-11 18:32:47 +0300

    Добавьте файлы проекта.

---
+++ 24/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("введите y:");
+double y = double.Parse(Console.ReadLine());
+if (y == Math.Pow(x, 2) - 2) Console.WriteLine("принадлежит");
+else if (y == x) Console.WriteLine("принадлежит");
+else if (y == (-x)) Console.WriteLine("принадлежит");
+else Console.WriteLine("не принадлежит");
